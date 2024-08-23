a152034e1c6960ed7178bd7c8b8dbf33d47163c5
Jiya Chowdhury <138868637+jc-2402@users.noreply.github.com>
2024-08-23 23:03:15 +0530

    Add files via upload

--- Queue10.java
+++ Queue10.java
@@ -3,6 +3,17 @@ import java.util.*;
 public class Queue10{
     public static void main(String[] args) {
         Deque <Integer> d = new LinkedList<>();
-        
+        d.addFirst(1);
+        d.addLast(2);
+        d.addFirst(4);
+        d.addLast(6);
+        d.addLast(3);
+        System.out.println(d);
+        d.removeLast();
+        d.removeFirst();
+        System.out.println(d);
+        System.out.println("first ele = "+d.peekFirst());
+        System.out.println("last ele = "+d.peekLast());
+
     }
 }
